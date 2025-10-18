9878f7cf7e0141b9b89195f544a321063c99ceec
Fidias2025-ECEN404 <fidiasalejandre726@tamu.edu>
2025-10-17 20:24:27 -0500

    Delete dev directory

--- dev/data/state_machine_data/xgb2cpp-master/sample/codeblocks_project/xgboost_classifier.h
+++
@@ -1,8 +0,0 @@
-#ifndef XGBOOST_CLASSIFIER_H  // To make sure you don't declare the function more than once by including the header multiple times.
-#define XGBOOST_CLASSIFIER_H
-
-#include <vector>
-
-std::vector<float> xgb_classify(std::vector<float> &sample);
-
-#endif
--- dev/data/state_machine_data/xgb2cpp.py
+++
@@ -1,5 +0,0 @@
-import xgboost as xgb
-
-bst = xgb.Booster()
-bst.load_model(r"C:\Users\fidia\OneDrive - Texas A&M University\Fall 2025\ECEN 404\Software\State machine\saved_models\3rd test\model_fold_1_2025-06-25_20-15-31.json")
-bst.dump_model("model_fold_1_2025-06-25_20-15-31.raw.txt", with_stats= False)
